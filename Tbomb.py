5e0b234ea5c9e7073a059fefd1565704c192c96a
armanhack-new <106769672+armanhack-new@users.noreply.github.com>
2022-06-03 02:33:23 +0600

    Arman.py
    Python

--- Tbomb.py
+++ Tbomb.py
@@ -1,7 +1,7 @@
-# ToxicBomber
+# Tarman bomber 
 # Tool : Unlimited SMS Bombing In Bangladeshi Numbers
-#Author : ToxicNoob
-# Coder : HunterSl4d3
+#Author : Arman
+# Coder : Arman
 
 import time
 import requests
@@ -55,15 +55,29 @@ def update():
 def logo():
     os.system("clear")
     print("\033[94m┌────────────────────────────────────────┐".center(columns+5))
-    print("\033[94m│     \033[92m▀▛▘     ▗    ▛▀▖       ▌        \033[94m   │".center(columns+15))
-    print("\033[94m│     \033[92m ▌▞▀▖▚▗▘▄ ▞▀▖▙▄▘▞▀▖▛▚▀▖▛▀▖▞▀▖▙▀▖\033[94m   │".center(columns+15))
-    print("\033[94m│     \033[92m ▌▌ ▌▗▚ ▐ ▌ ▖▌ ▌▌ ▌▌▐ ▌▌ ▌▛▀ ▌  \033[94m   │".center(columns+15))
-    print("\033[94m│     \033[92m ▘▝▀ ▘ ▘▀▘▝▀ ▀▀ ▝▀ ▘▝ ▘▀▀ ▝▀▘▘  \033[94m   │".center(columns+15))
+    print("\033[94m│     \033[92m  ___  _________  ___  ___   _   _ 
+
+ / _ \ | ___ \  \/  | / _ \ | \ | |
+
+/ /_\ \| |_/ / .  . |/ /_\ \|  \| |
+
+|  _  ||    /| |\/| ||  _  || . ` |
+
+| | | || |\ \| |  | || | | || |\  |
+
+\_| |_/\_| \_\_|  |_/\_| |_/\_| \_/
+
+                                   
+
+                                        \033[94m   │".center(columns+15))
+    print033[94m 
+    print("\033[94m│ \033[94m   │".center(columns+15))
+    print("\033[94m│     \033[92m \033[94m   │".center(columns+15))
     print("\033[94m│                              \033[94m          │".center(columns+9))
     print("\033[94m│ \033[95mAuthor : ToxicNoob                     \033[94m│".center(columns+15))
     print("│ \033[95mTool   : Unlimited SMS Bomber          \033[94m│".center(columns+9))
-    print("│ \033[95mGitHub : https://github.com/Toxic-Noob \033[94m│".center(columns+9))
-    print("│ \033[95mCoder  : HunterSl4d3              \033[37mV3.0 \033[94m│".center(columns+15))
+    print("│ \033[95mGitHub : https://github.com/armanhack-new/arman1.py \033[94m│".center(columns+9))
+    print("│ \033[95mCoder  : Arman            \033[37mV3.0 \033[94m│".center(columns+15))
     print("\033[94m└────────────────────────────────────────┘".center(columns+5))
 
 
